92ffc100110a19aff1048c3fd0f7d84369df3053
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-01-16 17:06:15 +0600

    feat: onchange function for is_child field based on age
    feat: onchange function for is_child field based on age

--- models/patient.py
+++ models/patient.py
@@ -7,7 +7,14 @@ class HospitalPatients(models.Model):
 
     name = fields.Char(string="Name", required=True, tracking=True)
     age = fields.Integer(string="Age", tracking=True)
-    is_child = fields.Boolean(string="Is Child")
+    is_child = fields.Boolean(string="Is Child", default=False)
     notes = fields.Text(string="Notes")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'),
                                ('other', 'Other')], string="Gender", tracking=True)
+
+    @api.onchange('age')
+    def _onchange_age(self):
+        if self.age<= 10:
+            self.is_child = True
+        else:
+            self.is_child = False

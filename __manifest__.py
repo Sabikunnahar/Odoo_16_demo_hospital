306e51abccb793295c916863964d9c5afe430f23
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-01-16 10:56:33 +0600

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hospital Management System',
+    'author': 'Sabikun Nahar Bristy',
+    'summary': 'For Hospital Management System',
+    'sequence': 2,
+    'website': 'https://www.odoo.com/app/invoicing',
+    # 'depends': ['base'],
+    'data': [
+
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+}

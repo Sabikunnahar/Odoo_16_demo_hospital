c4c565db930386f83bbf93d82435aa7fe3eb8b75
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-01-16 11:01:45 +0600

    feat: Menu creating with sub menus
    feat: Menu creating with sub menus

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,9 @@
     'website': 'https://www.odoo.com/app/invoicing',
     # 'depends': ['base'],
     'data': [
-
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,

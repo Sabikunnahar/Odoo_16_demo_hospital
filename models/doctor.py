9b38ff29af3a2fb045b9ba9a42b2c6e8fc119d8a
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-01-27 12:40:49 +0600

    Feat: This functionality specify a field while creating many2one relation
    Feat: This functionality specify a field while creating many2one relation

--- models/doctor.py
+++ models/doctor.py
@@ -5,6 +5,8 @@ class HospitalPatients(models.Model):
     _name = "hospital.doctor"
     _inherit = ['mail.thread']
     _description = "Hospital Doctors Records"
+    _rec_name = 'reference' #When we create many2one field by default it shows 'name' field but if I want to specify and field while creating the relation, we can do that by '_rec_name
+
 
     name = fields.Char(string="Doctor Name")
     reference = fields.Char(string="Reference")

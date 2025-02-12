27dbb7abd8e3e6a046f150cdd17d4eb8c405c21e
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-02-12 15:09:02 +0600

    Fix : Adding rec_name
    Fix : Adding rec_name

--- models/appointment.py
+++ models/appointment.py
@@ -4,9 +4,15 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread']
     _description = "Hospital appointment"
+    _rec_name = 'ref'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
+    ref = fields.Char(string='Reference')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Datetime(string='Booking Date', default=fields.Date.context_today)
     # This gender is the relational filed
     gender = fields.Selection( related='patient_id.gender')
+
+    @api.onchange('patient_id')
+    def _onchange_reference(self):
+        self.ref = self.patient_id.ref

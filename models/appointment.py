7f715072ac7f6ad6c04926abd2164fb4f08c9a56
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-05-07 15:18:15 +0600

    Feat: Image/Avatar in many2one relation

--- models/appointment.py
+++ models/appointment.py
@@ -13,6 +13,8 @@ class HospitalAppointment(models.Model):
     # This gender is the relational filed
     gender = fields.Selection( related='patient_id.gender')
     prescription = fields.Html(string='Prescription', tracking=True)
+    doctor_id = fields.Many2one('res.users', string='Doctor')
+
     # for priority widget batch
     priority = fields.Selection([
         ('0', 'Normal'),
